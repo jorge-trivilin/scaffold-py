570233c59ec28549ba4d8194fdab9b82e3bbf9b8
Jorge <jorge.trivilin@gmail.com>
2024-07-26 21:41:19 -0300

    Correcting lint problems

--- hello.py
+++ hello.py
@@ -1,10 +1,10 @@
 """
-This module provides a simple sum function
+This module provides one simple sum function.
 """
 
 def add(num1, num2):
-    """A simple add function"""
-    return num1 + num2 
+    """This is a an add function"""
+    return num1 + num2
 
 if __name__ == "__main__":
     print(add(1,1))
