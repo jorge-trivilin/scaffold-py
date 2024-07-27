50768234e837e3df98561423b716840edbce7c3a
Jorge Trivilin <jorge.trivilin@gmail.com>
2024-07-27 01:07:07 +0000

    Auto-format code

--- hello.py
+++ hello.py
@@ -2,9 +2,11 @@
 This module provides one simple sum function.
 """
 
+
 def add(num1, num2):
     """This is a an add function"""
     return num1 + num2
 
+
 if __name__ == "__main__":
-    print(add(1,1))
+    print(add(1, 1))
